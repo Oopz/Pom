28cbe4a772ed4492ff2556cfb593caad899ca793
gorebill <gorebill@163.com>
2013-02-21 00:58:07 +0800

    Initial Commit

---
+++ Pom/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pom
+//
+//  Created by Bill on 21/2/13.
+//  Copyright Oopz 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
